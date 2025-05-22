02cbac6cf7dfbc1613220053167495fe8d1f3b35
sheep289 <2890492738@qq.com>
2025-05-22 16:04:53 +0800

    feat: 完搜删除搜索历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -22,7 +22,18 @@
     <!-- 最近搜索 -->
     <div class="history">
       <span>最近搜索</span>
-      <van-icon name="delete-o" class="del-icon" />
+      <div v-show="serachHistory.length > 0">
+        <van-icon
+          name="delete-o"
+          class="del-icon"
+          @click="edit = false"
+          v-if="edit"
+        />
+        <div v-else class="edit-box">
+          <span @click="onDelAll">全部删除</span> &nbsp;|&nbsp;
+          <span class="fulfill" @click="edit = true">完成</span>
+        </div>
+      </div>
     </div>
     <div class="list">
       <div
@@ -31,7 +42,10 @@
         :key="index"
         @click="submitForm(item)"
       >
-        {{ item }}
+        <div class="text-box">
+          <div>{{ item }}</div>
+          <div class="delete-btn" :class="{active: edit}" @click="submitForm(item)"><van-icon name="cross"></van-icon></div>
+        </div>
       </div>
     </div>
   </div>
@@ -48,22 +62,49 @@ export default {
   data () {
     return {
       serachContent: '',
-      serachHistory: getHistoryList() // 搜索历史记录
+      serachHistory: getHistoryList(), // 搜索历史记录
+      edit: true
     }
   },
   methods: {
     submitForm (item) {
       // 获取重复的索引号，没有重复为-1
+      if (!item) return
       const index = this.serachHistory.indexOf(item)
+      // 删除逻辑
+      if (!this.edit) {
+        if (index !== -1) {
+          this.serachHistory.splice(index, 1)
+        }
+        setHistoryList(this.serachHistory)
+        if (this.serachHistory.length <= 0) this.edit = true
+        return
+      }
+      // 历史记录
       if (index !== -1) {
         this.serachHistory.splice(index, 1)
       }
-      // this.serachContent = item
-
       // 将搜索的内容添加到搜索历史数组中
       this.serachHistory.unshift(item)
       setHistoryList(this.serachHistory)
       // 跳转到搜索列表
+    },
+    onDelAll () {
+      this.$modal
+        .confirm({
+          message: '你确定删除全部历史记录？',
+          confirmButtonColor: '#ee0a24',
+          cancelBtnText: '取消',
+          confirmBtnText: '确定',
+          closerOnClickModal: false
+        })
+        .then(() => {
+          setHistoryList([])
+          this.serachHistory = []
+          this.edit = true
+          this.$emit('input', false) // 关闭底层弹框
+        })
+        .catch(() => {})
     }
   }
 }
@@ -139,18 +180,28 @@ export default {
   .history {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     margin: 18px 12px;
+    .edit-box {
+      color: #777;
+      font-size: 12px;
+      .fulfill {
+        color: #333;
+      }
+    }
   }
   .list {
     display: flex;
     flex-wrap: wrap;
-    gap: 5%;
+    gap: 2%;
     justify-content: flex-start;
     padding: 0 10px;
     .list-item {
-      width: 30%;
+      // width: 30%;
+      max-width: 36%;
+      min-width: 20px;
+      width: fit-content;
       text-align: center;
-      padding: 7px;
       line-height: 15px;
       border-radius: 50px;
       background: #fff;
@@ -160,6 +211,26 @@ export default {
       white-space: nowrap;
       text-overflow: ellipsis; //使用省略号表示溢出
       margin-bottom: 10px;
+      border: 1px solid #a1a1a1;
+      position: relative;
+      .text-box {
+        padding: 4px 8px;
+        .delete-btn {
+          background-color: rgb(165, 165, 165);
+          position: absolute;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          min-width: 20px;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          text-align: center;
+          &.active{
+          display: none;
+          }
+        }
+      }
     }
   }
 }
